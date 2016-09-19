10b0419492277dd74cb6f793aca3bb176a0bf8b0
dubynskyihw6 <pochta742@mail.ru>
2016-09-19 21:57:22 +0300

    end

--- JS1_2/js/script.js
+++ JS1_2/js/script.js
@@ -1,29 +1,4 @@
-﻿//
-// alert('Возведение числа x в степень n');
-// var resultat = inputData();
-// alert(resultat);
-// function inputData() {
-//     var basis = prompt("Введите значение числа");
-//     var index = Math.round(prompt("Введите число степени", ""));
-//     do {
-//         if (index <= 0) {
-//             alert('Степень' + index + 'не поддерживается, введите целую степень, большую 0');
-//             index = Math.round(prompt("Введите число степени", ""));
-//         }
-//     }
-//     while
-//         (index <= 0);
-//     function pow() {
-//         var result = basis;
-//         for (var i = 1; i < index; ++i) {
-//             result *= basis;
-//         }
-//         return result;
-//     }
-//
-//     return pow();
-//
-// }
+﻿
 (function () {
 
 
@@ -53,7 +28,7 @@
             }
 
             return result;
-        }
+        };
     }
 })();
 
